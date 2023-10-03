686e7abf734d48191735fbe0de680d4fedfceb89
JuanLaspiur <laspiurperezjuan@gmail.com>
2023-10-03 12:14:32 -0300

    Create Habitacion.java

---
+++ proyecto Area 51/src/modelo/Habitacion.java
@@ -0,0 +1,68 @@
+package modelo;
+/*Hecho por Juan Laspiur*/
+public class Habitacion {
+//atributos
+
+    private int idHabitacion;
+    private int idCategoria;
+    private int piso;
+    private int estado;
+//contructores 
+
+    public Habitacion() {
+    }
+
+    public Habitacion(int idCategoria, int piso, int estado) {
+        this.idCategoria = idCategoria;
+        this.piso = piso;
+        this.estado = estado;
+    }
+
+    public Habitacion(int idHabitacion, int idCategoria, int piso, int estado) {
+        this.idHabitacion = idHabitacion;
+        this.idCategoria = idCategoria;
+        this.piso = piso;
+        this.estado = estado;
+    }
+
+//metodos
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "idHabitacion=" + idHabitacion + ", idCategoria=" + idCategoria + ", piso=" + piso + ", estado=" + estado + '}';
+    }
+    
+    
+}
