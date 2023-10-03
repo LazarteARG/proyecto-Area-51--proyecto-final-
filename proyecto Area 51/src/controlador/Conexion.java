d56a7eda481be6b5a6fe477ba31a843a8d4a378d
LazarteARG <141943614+LazarteARG@users.noreply.github.com>
2023-10-03 12:46:20 -0300

    Update de la clase Conexion

--- proyecto Area 51/src/controlador/Conexion.java
+++ proyecto Area 51/src/controlador/Conexion.java
@@ -8,9 +8,9 @@ import javax.swing.JOptionPane;
 
 
 public class Conexion {
-    private static final String URL="jdbc:mariadb://localhost/";
-    private static final String DB=" ";
-    private static final String USUARIO="";
+    private static final String URL="jdbc:mariadb://localhost/hotelarea51";
+    private static final String DB="hotelarea51";
+    private static final String USUARIO="root";
     private static final String PASSWORD="";
     private static Connection connection;
     
@@ -21,12 +21,16 @@ public class Conexion {
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
                 connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "*Error*: hubo un error al cargar los drivers");
+                
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "*Error*: hubo un error de conectarse a la base de datos");
+                
             }
         }
+        
         return connection;
     }
 }
