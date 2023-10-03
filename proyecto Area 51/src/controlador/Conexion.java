ddcbe00b21c389cc913f9a171192aabe5b3f4f76
LazarteARG <141943614+LazarteARG@users.noreply.github.com>
2023-10-03 11:26:29 -0300

    Update de la clase Conexion
    Solo falta poner bien la URL de la base de datos, alguna contraseña, si quieren cambian el nombre de usuario y falta crear el metodo main en otro lado para poder hacer el return al metodo Main la siguiente linea de codigo: Connection con = Conexion.getConexion();

---
+++ proyecto Area 51/src/controlador/Conexion.java
@@ -0,0 +1,32 @@
+
+package controlador;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB=" ";
+    private static final String USUARIO="";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion () {}
+    
+    public static Connection getConexion() {
+        if (connection==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "*Error*: hubo un error al cargar los drivers");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "*Error*: hubo un error de conectarse a la base de datos");
+            }
+        }
+        return connection;
+    }
+}
