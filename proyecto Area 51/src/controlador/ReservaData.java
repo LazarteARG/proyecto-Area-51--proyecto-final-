8d22ab92697b6db1d7b30c2d11ffa957eec309f6
tamaracantero <141643686+tamaracantero@users.noreply.github.com>
2023-10-06 12:30:31 -0300

    se agrega el metodo actualizar reserva
    se agrega el metodo actualizarReserva a reservaData

--- proyecto Area 51/src/controlador/ReservaData.java
+++ proyecto Area 51/src/controlador/ReservaData.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Date;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,5 +26,22 @@ public static void subirReserva(Reserva r) {
     
 }     
 
- 
+ /**hecho por tamara*/
+//idReserva,idHuesped,idHabitacion,fechaEntrada,fechaSalida,
+//cantidadPersonas,montoAPagar,estado	
+
+public static void actualizarReserva(Reserva r){
+    sql="UPDATE reserva SET idHuesped=?,idHabitacion=?,fechaEntrada=?,fechaSalida=?,cantidadPersonas=?,montoAPagar=?,estado=? WHERE idReserva=?";
+    try{
+        ps=con.prepareStatement(sql);
+        ps.setInt(1, r.getHuesped().getIdHuesped());
+        ps.setInt(2, r.getHabitacion().getIdHabitacion());
+        ps.setDate(3, Date.valueOf(r.getFechaIngreso()));
+        ps.setDate(4, Date.valueOf(r.getFechaEgreso()));
+        ps.setInt(5, r.getCantPersonas());
+        ps.setDouble(6, r.getPrecioTotal());
+        
+        
+    }catch(Exception e){}
+}
 }
