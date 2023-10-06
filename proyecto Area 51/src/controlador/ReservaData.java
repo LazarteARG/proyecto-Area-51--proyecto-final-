d3eaf726b05090e20301c369b0bf9c97dae93ee9
LazarteARG <141943614+LazarteARG@users.noreply.github.com>
2023-10-06 11:53:56 -0300

    Create ReservaData.java

---
+++ proyecto Area 51/src/controlador/ReservaData.java
@@ -0,0 +1,29 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import modelo.Reserva;
+
+public class ReservaData {
+     private static Connection con = Conexion.getConexion();
+     private static String sql; 
+     private static PreparedStatement ps;
+     private static ResultSet rs;
+     
+public static void subirReserva(Reserva r) {
+    
+    
+    
+}     
+
+ 
+}
