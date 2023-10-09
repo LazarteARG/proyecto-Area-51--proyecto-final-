f637d83a4d8ce51b35b9a59862f9e0ea5481a918
tamaracantero <141643686+tamaracantero@users.noreply.github.com>
2023-10-09 12:06:22 -0300

    arregla error

--- proyecto Area 51/src/controlador/ReservaData.java
+++ proyecto Area 51/src/controlador/ReservaData.java
@@ -8,7 +8,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-import modelo.Reserva;
+import modelo.*;
 
 public class ReservaData {
 
