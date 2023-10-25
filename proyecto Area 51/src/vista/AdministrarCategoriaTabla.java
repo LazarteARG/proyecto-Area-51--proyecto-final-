c037b625cdb109495e20e25537d9c41483575525
JuanLaspiur <laspiurperezjuan@gmail.com>
2023-10-25 11:55:47 -0300

    qwer

---
+++ proyecto Area 51/src/vista/AdministrarCategoriaTabla.java
@@ -0,0 +1,81 @@
+package vista;
+
+import javax.swing.table.DefaultTableModel;
+
+
+public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
+  DefaultTableModel model = new DefaultTableModel();
+   
+    public AdministrarCategoriaTabla() {
+        initComponents();
+        tabla.setModel(model);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabla = new javax.swing.JTable();
+
+        setClosable(true);
+
+        tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tabla);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(57, 57, 57)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(62, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(76, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tabla;
+    // End of variables declaration//GEN-END:variables
+
+public void agregarCabeceraTabla(){
+model.addColumn("");
+model.addColumn("");
+model.addColumn("");
+model.addColumn("");
+model.addColumn("");
+
+   
+
+
+
+}
+
+
+
+
+
+
+}
