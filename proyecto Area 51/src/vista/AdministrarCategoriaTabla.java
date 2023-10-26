a50b1ffc1726b04865df642400beef837bc8cdd4
JuanLaspiur <laspiurperezjuan@gmail.com>
2023-10-26 12:29:42 -0300

    qwertyuio

--- proyecto Area 51/src/vista/AdministrarCategoriaTabla.java
+++ proyecto Area 51/src/vista/AdministrarCategoriaTabla.java
@@ -2,12 +2,19 @@ package vista;
 
 import controlador.CategoriaData;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import modelo.Categoria;
 
 public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
 
-    DefaultTableModel model = new DefaultTableModel();
+    DefaultTableModel model = new DefaultTableModel() {
+
+        @Override
+        public boolean isCellEditable(int f, int c) {
+            return false;
+        }
+    };
 
     public AdministrarCategoriaTabla() {
         initComponents();
@@ -25,11 +32,10 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
         jComboBox1 = new javax.swing.JComboBox<>();
         btnBuscar = new javax.swing.JButton();
         jTextField1 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         btnBuscarTodas = new javax.swing.JButton();
         btnEditar = new javax.swing.JButton();
         btnEliminar = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setClosable(true);
 
@@ -60,10 +66,6 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
             }
         });
 
-        jLabel1.setText("Buscar categoria:");
-
-        jLabel2.setText("según");
-
         btnBuscarTodas.setText("Listar Todo");
         btnBuscarTodas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -75,22 +77,14 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
 
         btnEliminar.setText("Eliminar");
 
+        jLabel3.setText("Buscar por");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jLabel1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel2)
-                        .addGap(4, 4, 4)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(37, 37, 37))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -99,24 +93,36 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(74, 74, 74)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnBuscar)
+                        .addGap(113, 113, 113)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(btnEditar)
-                    .addComponent(btnBuscar)
                     .addComponent(btnEliminar))
                 .addGap(29, 29, 29))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(47, 47, 47)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(btnBuscar)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel2))
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnBuscar)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(18, 18, 18)
@@ -126,7 +132,7 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
                         .addComponent(btnEditar)
                         .addGap(39, 39, 39)
                         .addComponent(btnEliminar)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
                 .addComponent(btnBuscarTodas)
                 .addGap(26, 26, 26))
         );
@@ -144,55 +150,62 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_btnBuscarTodasActionPerformed
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
-        String seleccionado = (String) jComboBox1.getSelectedItem();     
-        ArrayList<Categoria> listaCompleta =  CategoriaData.listarTodasLasCategorias();
-         ArrayList<Categoria> listaAMostrar = new  ArrayList<>();
-        
-        switch (seleccionado){
-            case "ID": 
-               vaciarTabla();
-               Categoria cate =  CategoriaData.obtenerCategoriaXId(Integer.parseInt(jTextField1.getText()));
-               agregarFila(cate);
+        String seleccionado = (String) jComboBox1.getSelectedItem();
+        ArrayList<Categoria> listaCompleta = CategoriaData.listarTodasLasCategorias();
+        ArrayList<Categoria> listaAMostrar = new ArrayList<>();
+
+        switch (seleccionado) {
+            case "ID":
+                vaciarTabla();
+                if (!jTextField1.getText().isBlank()) {
+                    Categoria cate = CategoriaData.obtenerCategoriaXId(Integer.parseInt(jTextField1.getText()));
+                    if (cate.getPrecioNoche() != 0.0) {
+                        agregarFila(cate);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(rootPane, "ID no encontrado en ninguna Categoria ");
+                }
                 break;
+
             case "Nombre":
-                  vaciarTabla();
-                  listaAMostrar = new  ArrayList<>();
-                  for (Categoria categoria : listaCompleta) {
-                    if(categoria.getNombre().startsWith(jTextField1.getText())){  
-                    listaAMostrar.add(categoria);
+                vaciarTabla();
+                listaAMostrar = new ArrayList<>();
+                for (Categoria categoria : listaCompleta) {
+                    if (categoria.getNombre().startsWith(jTextField1.getText())) {
+                        listaAMostrar.add(categoria);
                     }
                 }
                 cargarTabla(listaAMostrar);
                 break;
             case "TipoDeCamas":
                 vaciarTabla();
-                 listaAMostrar = new  ArrayList<>();
-                  for (Categoria categoria : listaCompleta) {
-                    if(categoria.getTipoDeCamas().startsWith(jTextField1.getText())){  
-                    listaAMostrar.add(categoria);
+                listaAMostrar = new ArrayList<>();
+                for (Categoria categoria : listaCompleta) {
+                    if (categoria.getTipoDeCamas().startsWith(jTextField1.getText())) {
+                        listaAMostrar.add(categoria);
                     }
                 }
-                cargarTabla(listaAMostrar);   
-               break;
+                cargarTabla(listaAMostrar);
+                break;
             case "CantidadDePersonas":
                 vaciarTabla();
-                 listaAMostrar = new  ArrayList<>();
-                  for (Categoria categoria : listaCompleta) {
-                    if(categoria.getTipoDeCamas().startsWith(jTextField1.getText())){  
-                    listaAMostrar.add(categoria);
+                listaAMostrar = new ArrayList<>();
+                for (Categoria categoria : listaCompleta) {
+                    if (categoria.getTipoDeCamas().startsWith(jTextField1.getText())) {
+                        listaAMostrar.add(categoria);
                     }
                 }
-                 cargarTabla(listaAMostrar);  
+                cargarTabla(listaAMostrar);
                 break;
             case "PrecioNoche":
-                  vaciarTabla();
-                 listaAMostrar = new  ArrayList<>();
-                  for (Categoria categoria : listaCompleta) {
-                    if(categoria.getPrecioNoche() == (Double.parseDouble(jTextField1.getText()))){  
-                    listaAMostrar.add(categoria);
+                vaciarTabla();
+                listaAMostrar = new ArrayList<>();
+                for (Categoria categoria : listaCompleta) {
+                    if (categoria.getPrecioNoche() == (Double.parseDouble(jTextField1.getText()))) {
+                        listaAMostrar.add(categoria);
                     }
                 }
-                 cargarTabla(listaAMostrar);  
+                cargarTabla(listaAMostrar);
                 break;
         }
 
@@ -205,8 +218,7 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnEliminar;
     private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTable tabla;
@@ -215,18 +227,19 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
     public void agregarCabeceraTabla() {
         model.addColumn("idCategoria");
         model.addColumn("Nombre");
+        model.addColumn("CantidadDeCamas");
         model.addColumn("TipoDeCamas");
         model.addColumn("CantidadPersonas");
         model.addColumn("PrecioNoche");
     }
 
     public void agregarFila(Categoria categoria) {
-        vaciarTabla();
-        model.addRow(new Object[]{categoria.getIdCategoria(), categoria.getNombre(), categoria.getTipoDeCamas(), categoria.getCantidadCamas(), categoria.getPrecioNoche()});
+        model.addRow(new Object[]{categoria.getIdCategoria(), categoria.getNombre(), categoria.getCantidadCamas(), categoria.getTipoDeCamas(), categoria.getCantidadCamas(), categoria.getPrecioNoche()});
     }
 
     public void cargarTabla(ArrayList<Categoria> listaCategoria) {
-           for (Categoria categoria : listaCategoria) {
+        vaciarTabla();
+        for (Categoria categoria : listaCategoria) {
             agregarFila(categoria);
         }
 
@@ -240,10 +253,3 @@ public class AdministrarCategoriaTabla extends javax.swing.JInternalFrame {
     }
 
 }
-
-
-
-
-
-
-
