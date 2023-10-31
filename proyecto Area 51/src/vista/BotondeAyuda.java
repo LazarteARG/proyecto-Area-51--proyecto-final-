2fafd7f24917933ecfbbf273980f2fa454ffe989
LazarteARG <141943614+LazarteARG@users.noreply.github.com>
2023-10-31 10:17:09 -0300

    daf

--- proyecto Area 51/src/vista/BotondeAyuda.java
+++ proyecto Area 51/src/vista/BotondeAyuda.java
@@ -31,21 +31,18 @@ public class BotondeAyuda extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jScrollBar1 = new javax.swing.JScrollBar();
-        jLabel3 = new javax.swing.JLabel();
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
-        jTextArea1.setRows(5);
-        jTextArea1.setText("Primero seria ir al boton superior a la izquierda que dice \"Reserva\" para que te salgan algunas opciones de menú para luego seleccionar\n\"Hacer Reserva\", seleccionas las fechas de entrada, de salida y la cantidad de personas se hospedan, después en \n\"Seleccion de Habitaciones\" si es la primera vez que se hospeda el huesped (Huesped Nuevo) hay un botón para registrarlo, después de \nseleccionar la habitación iriamos a la parte final de la reserva \"Registrar Reserva\" en donde aparecera un combo box ");
+        jTextArea1.setRows(6);
+        jTextArea1.setText("Primero seria ir al boton superior a la izquierda que dice \"Reserva\" para que te salgan algunas opciones de menú para luego seleccionar\n\"Hacer Reserva\", seleccionas las fechas de entrada, de salida y la cantidad de personas se hospedan, después en \n\"Seleccion de Habitaciones\" si es la primera vez que se hospeda el huesped (Huesped Nuevo) hay un botón para registrarlo, después de \nseleccionar la habitación iriamos a la parte final de la reserva \"Registrar Reserva\" en donde aparecera un combo box con los huespedes\nen donde puede seleccionar el que esta haciendo la reserva (Cualquier cosa use el boton \"Buscar Huesped\" para buscar mas facil)");
 
         jLabel1.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
         jLabel1.setText("AYUDA: COMO FUNCIONA EL PROGRAMA");
 
         jLabel2.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         jLabel2.setText("1° El orden de como hacer una reserva:");
-
-        jLabel3.setText("2° Como funciona el \"Hacer Reserva\"");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -61,10 +58,7 @@ public class BotondeAyuda extends javax.swing.JInternalFrame {
                         .addComponent(jLabel2))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(38, 38, 38)
-                        .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 652, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3)))
+                        .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 652, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addComponent(jScrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -80,9 +74,7 @@ public class BotondeAyuda extends javax.swing.JInternalFrame {
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3)
-                        .addGap(0, 291, Short.MAX_VALUE)))
+                        .addGap(0, 315, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -93,7 +85,6 @@ public class BotondeAyuda extends javax.swing.JInternalFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollBar jScrollBar1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
